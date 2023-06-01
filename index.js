43141dc23163fc6551473c3115bc6f91a3760729
Millicent Ama Adjubel <adjubelmillicent@gmail.com>
2023-06-01 00:10:22 +0000

    Enyata Final Project

---
+++ index.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form')
+const formName = document.getElementById('name')
+const formEmail = document.getElementById('email')
+const error = document.querySelector('.error')
+
+  //adding an event listener to perform a function when submit is clicked!
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    if(formName.value === '' ||  formEmail.value === ''){
+        error.style.display = 'block'
+
+        setTimeout(() => {
+            error.style.display = 'none'
+        }, 5000) 
+        // 5000 milliseconds=5secs meaning after 5 seconds the error message should be gone
+
+
+        return
+    }
+    alert(`Hello ${formName.value}🎊, your form has been submitted successfully.\nYour email is ${formEmail.value}`)
+    console.log(formName.value)
+    console.log(formEmail.value)
+
+    formName.value = ''
+    formEmail.value = ''
+
+    
+})
